4b7ea14eff72394f90bdd40f07127291bd58c78d
[suyash patel] <suyashpatelmrj@gmail.com>
2021-04-19 03:24:54 +0530

    address

--- signup_script.php
+++ signup_script.php
@@ -6,7 +6,7 @@ $password=$_GET["password"];
 $contact=$_GET["contact"];
 $city=$_GET["city"];
 $address=$_GET["address"];
-$user_registration_query="INSERT into signup(name,email,password,contact,city,address) values ('$name','$email','$password','$contact','$city','address')";
+$user_registration_query="INSERT into signup(name,email,password,contact,city,address) values ('$name','$email','$password','$contact','$city','$address')";
 $user_registration_submit=mysqli_query($con,$user_registration_query) or die(mysqli_error($con));
 echo "<script> window.location.assign('login.php'); </script>";
 ?>
